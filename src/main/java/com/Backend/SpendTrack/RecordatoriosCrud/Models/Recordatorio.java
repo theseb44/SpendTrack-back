9e2f9ddf5fad9ab4072904319c1ee8228745d2ed
Sebastian <theseb242@gmail.com>
2023-05-23 19:54:52 -0500

    Crud Recordatorios

---
+++ src/main/java/com/Backend/SpendTrack/RecordatoriosCrud/Models/Recordatorio.java
@@ -0,0 +1,53 @@
+package com.Backend.SpendTrack.RecordatoriosCrud.Models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "recordatorio")
+public class Recordatorio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "id_cliente")
+    private Long idCliente;
+
+    @Column(name = "titulo")
+    private String titulo;
+
+    @Column(name = "descripcion")
+    private String descripcion;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(Long idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
